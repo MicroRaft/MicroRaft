44c890f8e72ca52e5859f2781d1d4fdfb831e8ef
Basri Kahveci <ebkahveci@gmail.com>
2023-01-27 20:22:38 +0000

    Move afloatdb to another repo

--- afloatdb/afloatdb-client/src/main/java/io/microraft/afloatdb/client/kv/Ordered.java
+++
@@ -1,9 +0,0 @@
-package io.microraft.afloatdb.client.kv;
-
-import java.util.function.Supplier;
-
-public interface Ordered<T> extends Supplier<T> {
-
-    long getCommitIndex();
-
-}
--- afloatdb/afloatdb-server/src/main/java/io/microraft/afloatdb/internal/raft/impl/model/message/RaftRequestAware.java
+++
@@ -1,9 +0,0 @@
-package io.microraft.afloatdb.internal.raft.impl.model.message;
-
-import io.microraft.afloatdb.raft.proto.RaftRequest;
-
-public interface RaftRequestAware {
-
-    void populate(RaftRequest.Builder builder);
-
-}

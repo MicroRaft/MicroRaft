5fe7985fcf940f3cc16112980af01d7d94e5ce5b
Lukas Brand <lukas.brand@hivemq.com>
2025-05-30 16:22:28 +0200

    WIP: Add Gradle wrapper and root scaffold

---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+}
+
+rootProject.name = "MicroRaft"
+include("microraft")
+include("microraft-hocon")
+include("microraft-metrics")
+include("microraft-store-sqlite")
+include("microraft-tutorial")
+include("microraft-yaml")

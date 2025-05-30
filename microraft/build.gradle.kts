ebd3ac48d506caa69f5dc2874a9f716a4c3b8ff9
Lukas Brand <lukas.brand@hivemq.com>
2025-05-30 16:33:07 +0200

    WIP: Port microraft module to Gradle: - Introduce test fixtures to share test code across project to other modules - Replicate current configuration from Maven - Explicit in-module configuration - Add additional information to Jar file (Title, Vendor, Version)
    Missing:
    - Publishing
    - Staging
    - Checksum

---
+++ microraft/build.gradle.kts
@@ -0,0 +1,94 @@
+plugins {
+    `java-library`
+    `java-test-fixtures`
+    alias(libs.plugins.defaults)
+    alias(libs.plugins.metadata)
+}
+
+group = "io.microraft"
+version = "0.9-SNAPSHOT"
+description = "Feature-complete implementation of the Raft consensus algorithm"
+
+metadata {
+    moduleName.set("io.microraft.microraft")
+    readableName.set("Microraft")
+    license {
+        apache2()
+    }
+    organization {
+        name.set("MicroRaft")
+        url.set("https://microraft.io")
+    }
+    developers {
+        register("metanet") {
+            fullName.set("Ensar Basri Kahveci")
+            email.set("ebkahveci@gmail.com")
+        }
+        register("mdogan") {
+            fullName.set("Mehmet Dogan")
+            email.set("mehmet@dogan.io")
+        }
+    }
+    github {
+        org.set("MicroRaft")
+        pages()
+        issues()
+    }
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(11)
+    }
+    withJavadocJar()
+    withSourcesJar()
+}
+
+tasks.withType<Jar>().configureEach {
+    manifest.attributes(
+        "Implementation-Title" to project.name,
+        "Implementation-Vendor" to metadata.organization.provider.flatMap { it.name },
+        "Implementation-Version" to provider { project.version.toString() },
+    )
+}
+
+tasks.javadoc {
+    isFailOnError = false
+}
+
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly(libs.findbugs)
+    implementation(libs.slf4j)
+}
+
+
+@Suppress("UnstableApiUsage") //
+testing {
+    suites {
+        withType(JvmTestSuite::class) {
+            useJUnit(libs.versions.junit)
+        }
+
+        val test by getting(JvmTestSuite::class) {
+            dependencies {
+                implementation(libs.mockito)
+                implementation(libs.assertj)
+                implementation(libs.log4j.core)
+                implementation(libs.log4j.api)
+                implementation(libs.log4j.slf4j.impl)
+                compileOnly(libs.findbugs)
+            }
+        }
+    }
+}
+
+dependencies {
+    testFixturesImplementation(libs.junit)
+    testFixturesCompileOnly(libs.findbugs)
+    testFixturesImplementation(libs.slf4j)
+}

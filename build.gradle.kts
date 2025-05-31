a2591a0fe8edd6a6b05bbc690ab3059d27cf7857
Lukas Brand <lukas.brand@hivemq.com>
2025-05-31 02:21:44 +0200

    Add todos for root project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,13 @@
-allprojects {
-    apply { plugin("checkstyle") }
+plugins {
+    id("com.github.spotbugs") version "6.1.13" apply false
 }
+
+allprojects {
+//    plugins.apply("checkstyle")
+//    plugins.apply("com.github.spotbugs")
+}
+
+//TODO NOTICE and LICENSE are not inside the jar files
+//TODO Formatter is not in place
+//TODO Spotbugs
+//TODO Checkstyle

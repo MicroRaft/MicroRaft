245d07aeeee0296a794d2e5f68672dc32aedfb08
Silvio Giebl <gieblsilvio@outlook.com>
2025-06-03 18:04:56 +0200

    Move dependencyResolutionManagement under subproject includes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,15 +1,15 @@
 rootProject.name = "MicroRaft"
 
-dependencyResolutionManagement {
-    @Suppress("UnstableApiUsage") //
-    repositories {
-        mavenCentral()
-    }
-}
-
 include("microraft")
 include("microraft-hocon")
 include("microraft-metrics")
 include("microraft-store-sqlite")
 include("microraft-tutorial")
 include("microraft-yaml")
+
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage") //
+    repositories {
+        mavenCentral()
+    }
+}
